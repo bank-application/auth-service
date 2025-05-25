f67afdd54a77b3a5155b2511422e46fcf2f4299d
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-05-25 17:16:37 +0530

    initial commit

---
+++ src/test/java/com/bank/auth/service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
